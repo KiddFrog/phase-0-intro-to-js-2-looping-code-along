e908d1817f130845436dbefaf1f7c3ee4d636c8c
domm <dommferger@gmail.com>
2023-08-13 16:17:01 -0400

    for while loops

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+const occasion = "surprise";
+
+function writeCards(names, occasion) {
+    let thanks = [];
+    for (let i = 0; i < names.length; i++) {
+        thanks.push(`Thank you, ${names[i]}, for the wonderful ${occasion} gift!`);
+    }
+    return thanks;
+}
+
+const thankYouMessages = writeCards(names, occasion);
+console.log(thankYouMessages);
+
+
+// Count down part
+function countDown(number = 10){
+while (number >= 0) {
+    console.log(number);
+    number--
+}
+return countDown
+}
